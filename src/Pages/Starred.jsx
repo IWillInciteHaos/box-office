b48484eb4dc99f6e3568a82c0a8ea1a1d80c3017
Angelina Ivanovic <angelina.gina2@gmail.com>
2023-02-27 21:21:06 +0100

    starred page is functional
    no pretty pages still...

--- src/Pages/Starred.jsx
+++ src/Pages/Starred.jsx
@@ -1,10 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMultipleShowsById } from "../api/tvmaze";
+import ShowGrid from "../components/shows/ShowGrid";
 import { useStarredShows } from "../lib/UseStarredShows";
 
 const Starred = () => {
 
-    const [starredShows, dispatchStarred] = useStarredShows();
+    const [starredShowsIds] = useStarredShows();
 
-    return <div>Starred page, starred: {starredShows.length}</div>
+    const { data : starredShows, error : starredShowsError} = useQuery({
+        queryKey: ['starred', starredShowsIds],
+        queryFn: () => getMultipleShowsById(starredShowsIds).then(result => 
+            result.map(show => ({ show }))),
+        refetchOnWindowFocus: false
+    });
+
+    if(starredShows?.length > 0) {
+        return <ShowGrid shows={starredShows} />
+    }else if(starredShows?.length === 0){
+        return <div>No starred shows</div>
+    }
+
+    if(starredShowsError){
+        return <div>ERROR: {starredShowsError.message}</div>
+    }
+
+    return <div>Shows loading...{starredShowsIds.length}</div>
 }
 
 export default Starred;
--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -10,3 +10,9 @@ const apiGet = async (queryString) => {
 export const searchForShows = (query) => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = (query) => apiGet(`/search/people?q=${query}`);
 export const getShowsByID = (showID) => apiGet(`/shows/${showID}?embed[]=seasons&embed[]=cast`)
+
+export const getMultipleShowsById = async (showIDs) => {
+    
+    const promises = showIDs.map(showId => apiGet(`/shows/${showId}`));            
+    return  Promise.all(promises);
+}
